df7bf7640cf011f088bae40966d4eea347275fff
manav <manavjhamb002@gmail.com>
2023-04-27 13:53:33 +0530

    fuuuuuuuuuuuuuuuuuuuuuuuuuuuck

--- server.js
+++ server.js
@@ -29,7 +29,34 @@ app.use(cookieParser());
 // 	exposedHeaders: ['Content-Type']
 // };
 
-app.use(cors());
+const corsMiddleware = (req, res, next) => {
+	res = applyCorsHeaders(res);
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
+		return;
+	}
+	next();
+};
+
+const applyCorsHeaders = (res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://talkspacee.netlify.app'
+	);
+	// or res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
+	return res;
+};
+
+app.use(corsMiddleware);
 
 app.use('/storage', express.static('storage'));
 
